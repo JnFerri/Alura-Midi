f64198431b9b9a9ef57d3555d4ca280d67c8fdc0
Jean Ferri <jeanferrimh@gmail.com>
2022-10-31 19:56:11 -0300

    Finalização

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 var listaTeclas =[]
 var listaAudios = []
-var audios = [document.querySelectorAll('audio')].map(valor => valor.forEach(audio=> listaAudios.push(audio)))
-var teclas = [document.querySelectorAll('.tecla')].map(valor => valor.forEach(tecla=>listaTeclas.push(tecla)))
+new Array(document.querySelectorAll('audio')).map(valor => valor.forEach(audio=> listaAudios.push(audio)))
+new Array(document.querySelectorAll('.tecla')).map(valor => valor.forEach(tecla=>listaTeclas.push(tecla)))
 
 listaTeclas.forEach(tecla => tecla.addEventListener('click', function(){
+    if(listaTeclas.indexOf(tecla) > listaAudios.length - 1){
+        alert('Som não encontrado, Necessario adicionar audio ao HTML')
+    }else{
     listaAudios[listaTeclas.indexOf(tecla)].play()
+    }
+    
 }))
 
+listaTeclas.forEach(tecla => tecla.onkeydown = function(event){
+    if(event.code == 'Enter' ||event.code == 'Space' ){
+    tecla.classList.add('ativa')
+    }
+})
+
+listaTeclas.forEach(tecla => tecla.onkeyup = function(event){
+    if(event.code== 'Enter' || event.code == 'Space'){
+    tecla.classList.remove('ativa')
+    }
+})
 
 
 
